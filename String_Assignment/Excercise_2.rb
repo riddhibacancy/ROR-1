3827f6f3998a5f9c464733e2b560bb76e3e0afc2
Riddhi Mehta <riddhi.mehta@bacancy.com>
2022-03-04 13:21:40 +0530

    Update Excercise_2.rb

--- String_Assignment/Excercise_2.rb
+++ String_Assignment/Excercise_2.rb
@@ -1,4 +1,2 @@
 sentence = "       This   is   my  ruby string     practical  assignment  "   
-last_word = sentence.split.last
-puts "Last word is : #{last_word}"
-puts "Length of last word is :#{last_word.length}"
+sentence.split.last.length
